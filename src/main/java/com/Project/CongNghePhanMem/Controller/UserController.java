584df6bfce174eb8979f70e06c6d68b06b88a44a
Thanh An <Nguyenthanhan26.qngai@gmail.com>
2024-11-23 14:37:22 +0700

    hoan thien template

--- src/main/java/com/Project/CongNghePhanMem/Controller/UserController.java
+++ src/main/java/com/Project/CongNghePhanMem/Controller/UserController.java
@@ -3,7 +3,6 @@ package com.Project.CongNghePhanMem.Controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
