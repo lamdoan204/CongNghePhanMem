2929a867bcf9e6fc5ece1aa7908c7568a235c5eb
lamdoan204 <22133032@student.hcmute.edu.vn>
2024-11-25 08:21:11 +0700

    Update CustomSuccessHandler.java

--- src/main/java/com/Project/CongNghePhanMem/configs/CustomSuccessHandler.java
+++ src/main/java/com/Project/CongNghePhanMem/configs/CustomSuccessHandler.java
@@ -26,7 +26,7 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 		} else if (roles.contains("ROLE_USER")) {
 			response.sendRedirect("/user/");
 		}else if (roles.contains("ROLE_EMPLOYEE")) {
-			response.sendRedirect("/manager/");
+			response.sendRedirect("/employee/");
 		}
 		else {
 			response.sendRedirect("/access-denied");
