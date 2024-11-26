1779a1c9dfef6b7b80de6fbfd67c445fa5823fab
lamdoan204 <22133032@student.hcmute.edu.vn>
2024-11-27 01:35:21 +0700

    cập nhật lại: truy cập được quản lý nhân viên và quản lý sản phẩm

--- src/main/java/com/Project/CongNghePhanMem/Controller/ManagerController.java
+++ src/main/java/com/Project/CongNghePhanMem/Controller/ManagerController.java
@@ -3,8 +3,6 @@ package com.Project.CongNghePhanMem.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.Project.CongNghePhanMem.Entity.Brand;
 import com.Project.CongNghePhanMem.Entity.Department;
 import com.Project.CongNghePhanMem.Entity.User;
 import com.Project.CongNghePhanMem.Service.BrandService;
